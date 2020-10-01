c0450df73dcef9d9f1fe52e00b6efef15e0fe755
Alagammai <lakshminarayanan887@gmail.com>
2020-10-01 13:27:09 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   
   title = 'Todo Application in Angular';
-  tasklist: any[] = [];
+  tasklist: string[] = [];
   searchtask: string;
   
 
